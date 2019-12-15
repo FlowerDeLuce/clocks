1207a0c0ccfbbdff148eda30dea46ec7b96982f8
daria.shapovalova <daria.shapovalova@ideus.biz>
2019-12-15 23:18:55 +0200

    layout

---
+++ assets/js/scripts.js
@@ -0,0 +1,31 @@
+'use strict';
+
+$(window).on('resize', function () {});
+
+$(window).on('load', function () {
+  initCollectionSlider();
+});
+
+function initCollectionSlider() {
+  $('.js-collectionSlider').each(function () {
+    $(this).slick({
+      prevArrow: '<span class="b-collection__arrow -prev"> <i class="fa fa-chevron-left"></i> </span>',
+      nextArrow: '<span class="b-collection__arrow -next"> <i class="fa fa-chevron-right"></i> </span>',
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+
+      responsive: [{
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+
+        }
+      }]
+    });
+  });
+}
